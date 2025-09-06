2acc01d7cc500e8c34eba7459cfdb22cd6b90315
Brayan-Fon <brayanstevenfontechalozano@gmail.com>
2025-09-05 19:40:32 -0500

    Update estructural.py

--- parcial-3/estructural.py
+++ parcial-3/estructural.py
@@ -4,8 +4,8 @@
 def cuenta_pares(lista):
     contador = 0
     for n in lista:
-        if n % 2 = 0:
+        if n % 2 == 0:
             contador += 1
     return contador
 
-print(cuenta_pares([1,2,3,4,5,6]))
+print(cuenta_pares([1, 2, 3, 4, 5, 6]))
